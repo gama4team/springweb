885b65c65d84dde1d1a09981ca8f7b32e2d5154b
willian <willianr.demelo@gmail.com>
2021-10-27 23:43:56 +0000

    springweb

--- build_jar.sh
+++ build_jar.sh
@@ -1,5 +1,5 @@
 export USER='root' 
-export PASSWORD='root' 
+export PASSWORD='mudando' 
 export DATABASE_URL='mysql://localhost:3306/SpringWebYoutubeTest?useTimezone=true&serverTimezone=UTC'
 
 mvn package
